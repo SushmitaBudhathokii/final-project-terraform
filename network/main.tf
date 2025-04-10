ab9a4d77b076fcc8c95c9dcfa78a3658cb7439e0
EC2 Default User <ec2-user@ip-172-31-18-6.ec2.internal>
2025-04-10 05:24:00 +0000

    Test gitignore with tf files

--- network/main.tf
+++ network/main.tf
@@ -1,7 +1,7 @@
 # Call the networkmodule to create network resources for the prod environment.
 
 module "network" {
-  source = "../../networkmodule" # Path to the networkmodule
+  source = "../networkModule" # Path to the networkmodule
 
   env = var.env # Pass the env variable to the network module
 }
