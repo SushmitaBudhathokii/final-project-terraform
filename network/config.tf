3fc8516e7d0005c0c20447e38651e5e6112d3123
SushmitaBudhathokii <127125737+SushmitaBudhathokii@users.noreply.github.com>
2025-04-15 22:25:26 -0400

    Update config.tf

--- network/config.tf
+++ network/config.tf
@@ -1,7 +1,7 @@
 # Remote state S3 bucket
 terraform {
   backend "s3" {
-    bucket  = "final-project-s3"
+    bucket  = "final-project-acs-s3"
     key     = "network/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
