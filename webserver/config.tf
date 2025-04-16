53d92c7f6064cd15ea781906b947b0020cd348b7
SushmitaBudhathokii <127125737+SushmitaBudhathokii@users.noreply.github.com>
2025-04-15 22:09:20 -0400

    Update config.tf

--- webserver/config.tf
+++ webserver/config.tf
@@ -1,7 +1,7 @@
 # Configure backend to store Terraform state in S3 bucket
 terraform {
   backend "s3" {
-    bucket  = "final-project-terraform-staging3"
+    bucket  = "final-project-terraform-staging2"
     key     = "webserver/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
